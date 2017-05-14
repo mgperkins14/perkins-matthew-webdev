3b1df1861cd5d0c4d478a8d9b0b355dc26f1d68b
Matthew Perkins <perkins.ma@husky.neu.edu>
2017-05-14 19:02:26 -0400

    Added environment check to construct connection string in app.js

--- public/test/app.js
+++ public/test/app.js
@@ -1,4 +1,13 @@
 (function() {
+
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+	var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+	var password = process.env.MLAB_PASSWORD_WEBDEV;
+	connectionString = 'mongodb://' + username + ':' + password;
+	connectionString += '@ds151279.mlab.com:51279/heroku_6gg9qzq5'; // user yours
+}
+    
     angular
         .module("TestApp", [])
         .controller("TestController", TestController)
