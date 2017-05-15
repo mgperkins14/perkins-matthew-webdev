de2ddbbcd5118ea83f566e9761a3e7744d2a9e01
Matt Perkins <perkins.ma@husky.neu.edu>
2017-05-15 13:09:07 -0400

    overrode connection string

--- test/app.js
+++ test/app.js
@@ -9,9 +9,9 @@ module.exports = function(app)
         var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
         var password = process.env.MLAB_PASSWORD_WEBDEV;
         connectionString = 'mongodb://' + username + ':' + password;
-        connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+        connectionString += '@ds151279.mlab.com:51279/heroku_6gg9qzq5'; // user yours
     }
-
+    
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
