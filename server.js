ff3a92bbb0d81d2895cfa75cae8db1d2b14d4def
Matt Perkins <perkins.ma@husky.neu.edu>
2017-05-16 13:10:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,16 @@ app.use(express.static(__dirname + '/public'));
 //require ("./test/app.js")(app);
 
 app.get('/env', function(req, res) {
-  res.json(process.env);
+//  res.json(process.env);
+  
+  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+  if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+      var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+      var password = process.env.MLAB_PASSWORD_WEBDEV;
+      connectionString = 'mongodb://' + username + ':' + password;
+      connectionString += '@ds151279.mlab.com:51279/heroku_6gg9qzq5'; // user yours
+  }
+  res.json({connectionString: connectionString});
 });
 
 var port = process.env.PORT || 3000;
