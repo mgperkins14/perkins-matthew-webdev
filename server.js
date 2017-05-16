adbd6c04990e8094df172f65fae22dca9fae51ba
Matt Perkins <perkins.ma@husky.neu.edu>
2017-05-16 13:17:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-//require ("./test/app.js")(app);
+require ("./test/app.js")(app);
 
 app.get('/env', function(req, res) {
 //  res.json(process.env);
@@ -21,13 +21,13 @@ app.get('/env', function(req, res) {
       connectionString += '@ds151279.mlab.com:51279/heroku_6gg9qzq5'; // user yours
   }
  
-  var mongoose = require('mongoose');
+//   var mongoose = require('mongoose');
   var error = null;
-  try {
-  mongoose.connect(connectionString);
-  } catch(e) {
-    error = e;
-  }
+//   try {
+//   mongoose.connect(connectionString);
+//   } catch(e) {
+//     error = e;
+//   }
   
   res.json({connectionString: connectionString, error: error});
 });
