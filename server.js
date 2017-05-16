a9c0f005f50d21a9711c601ad2917d66414faff4
Matt Perkins <perkins.ma@husky.neu.edu>
2017-05-16 13:20:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,28 +10,6 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-app.get('/env', function(req, res) {
-//  res.json(process.env);
-  
-  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-  if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-      var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-      var password = process.env.MLAB_PASSWORD_WEBDEV;
-      connectionString = 'mongodb://' + username + ':' + password;
-      connectionString += '@ds151279.mlab.com:51279/heroku_6gg9qzq5'; // user yours
-  }
- 
-//   var mongoose = require('mongoose');
-  var error = null;
-//   try {
-//   mongoose.connect(connectionString);
-//   } catch(e) {
-//     error = e;
-//   }
-  
-  res.json({connectionString: connectionString, error: error});
-});
-
 var port = process.env.PORT || 3000;
 
 app.listen(port);
